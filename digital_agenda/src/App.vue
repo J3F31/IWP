f1b1f41c10647927641b144030a60b457b503758
J3F31 <jero.eyras@gmail.com>
2022-12-07 16:58:43 +0100

    init

---
+++ digital_agenda/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ExamplePage />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ExamplePage from './components/ExamplePage.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ExamplePage
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ digital_agenda/src/components/ExamplePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>Example</h1>
+    <p>This will be a digital agenda</p>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ExamplePage",
+});
+</script>
