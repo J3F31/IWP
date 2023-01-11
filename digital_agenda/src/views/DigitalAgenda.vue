532c5b9183be52a9308d281c15bc218a1368171d
J3F31 <jero.eyras@gmail.com>
2023-01-11 01:44:26 +0100

    new text

--- digital_agenda/src/views/DigitalAgenda.vue
+++ digital_agenda/src/views/DigitalAgenda.vue
@@ -4,11 +4,9 @@
     <div class="poster">
       <section>
         <h1>Professional Engagement</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque modi asperiores laboriosam
-          quos perspiciatis animi cum fugiat mollitia similique magnam explicabo accusantium voluptas recusandae,
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin nisl a arcu fermentum sodales.
-          Aliquam eu quam diam. Mauris ut augue vitae est dapibus pulvinar. Aliquam id euismod nisi, at rhoncus turpis. 
-          Morbi nec leo eget ligula feugiat pulvinar vel  sit earum, maxime consequatur nobis provident!
+        <p>
+          Digital <strong>Communication</strong> a basic set of rules you should follow to make the Internet a <strong>better place for yourself</strong> and for others. 
+          Just as there are rules of etiquette in real life, face-to-face encounters, there are also rules you should follow while online.
         </p>
       </section>
       <img class="infographic" src="../../public/assets/Infographic_Communication.png" alt="Infographic Communication" loading="lazy">
@@ -17,11 +15,11 @@
     <div class="poster">
       <section>
         <h1>Digital Resources</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque modi asperiores laboriosam
-          quos perspiciatis animi cum fugiat mollitia similique magnam explicabo accusantium voluptas recusandae,
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin nisl a arcu fermentum sodales.
-          Aliquam eu quam diam. Mauris ut augue vitae est dapibus pulvinar. Aliquam id euismod nisi, at rhoncus turpis. 
-          Morbi nec leo eget ligula feugiat pulvinar vel  sit earum, maxime consequatur nobis provident!
+        <p>
+          Educators are currently confronted with a wealth of <strong>digital (educational) resources</strong> they can use for teaching. One of the 
+          key competences any educator needs to develop is to come to terms with this variety, to effectively identify resources 
+          that best fit their learning objectives, learner group and teaching style, to structure the wealth of materials, establish 
+          connections and to modify, add on to and develop themselves digital resources to support their teaching.
         </p>
       </section>
       <img class="infographic" src="../../public/assets/Infographic_Tools.png" alt="Infographic Tools" loading="lazy">
@@ -30,11 +28,14 @@
     <div class="poster">
       <section>
         <h1>Digital Security</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque modi asperiores laboriosam
-          quos perspiciatis animi cum fugiat mollitia similique magnam explicabo accusantium voluptas recusandae,
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin nisl a arcu fermentum sodales.
-          Aliquam eu quam diam. Mauris ut augue vitae est dapibus pulvinar. Aliquam id euismod nisi, at rhoncus turpis. 
-          Morbi nec leo eget ligula feugiat pulvinar vel  sit earum, maxime consequatur nobis provident!
+        <p>
+          Digital technologies can enhance and improve teaching and learning strategies in many different ways. However, whatever 
+          pedagogic strategy or approach is chosen, the educator’s specific digital competence lies in effectively orchestrating the use 
+          of digital technologies in the different phases and settings of the learning process.
+          <br><br>
+          At the same time they need to be aware of how to responsibly use and manage digital content. They must respect 
+          copyright rules when using, modifying and sharing resources, and protect sensitive content and data, such as digital 
+          exams or students’ grades.
         </p>
       </section>
       <img class="infographic" src="../../public/assets/Infographic_Security.png" alt="Infographic Security" loading="lazy">
